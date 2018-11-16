ec93a8f90865a4c054dbc84a845b683623fc0bfe
Niklas Sombert <niklas@ytvwld.de>
2018-11-16 19:53:18 +0100

    Create post model.

--- messageboard/models.py
+++ messageboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    """Our core thing - a single message."""
+    
+    sent = models.DateTimeField(
+        null=False,
+        auto_now_add=True,
+        help_text="The time when this post was created."
+    )
+    title = models.CharField(
+        max_length=500,
+        null=True,
+        help_text="The post's title."
+    )
+    content = models.TextField(
+        null=False,
+        help_text="The post's content."
+    )
