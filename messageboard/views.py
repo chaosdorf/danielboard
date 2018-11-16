d5365c2974d6e3c6cd7fa4d5631e5f8bd5f1dbf5
Niklas Sombert <niklas@ytvwld.de>
2018-11-16 19:29:30 +0100

    Add URLs.

--- messageboard/views.py
+++ messageboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def overview(request):
+    return HttpResponse("Here be messages.")
+
+
+def submit(request):
+    return HttpResponse("This creates a new message.")
